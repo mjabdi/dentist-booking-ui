8d2d9163c728dc7208b9f6bff871367d5f2e3515
mjabdi <m.jafarabdi@gmail.com>
2023-05-17 18:02:25 +0330

    19 20 aug

--- src/DateForm.js
+++ src/DateForm.js
@@ -157,6 +157,8 @@ export default function DateForm() {
                             || dateformat(date, 'yyyy-mm-dd') === "2023-04-23"                       
                             || dateformat(date, 'yyyy-mm-dd') === "2023-06-24"                       
                             || dateformat(date, 'yyyy-mm-dd') === "2023-06-25"                       
+                            || dateformat(date, 'yyyy-mm-dd') === "2023-08-19"                       
+                            || dateformat(date, 'yyyy-mm-dd') === "2023-08-20"                       
                     
 
 
